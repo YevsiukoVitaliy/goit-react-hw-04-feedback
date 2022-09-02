799d9a021eb61b1f546dd06ea58cf469057cf9fc
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2022-09-02 03:36:00 +0300

    df

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import './App.css';
+import css from './App.module.css';
 import Section from 'components/Section/Section';
 
 function App() {
   return (
     <>
-      <div
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          display: 'flex',
-        }}
-      >
+      <div className={css.App}>
         <Section />
       </div>
     </>
